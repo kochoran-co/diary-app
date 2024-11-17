6369e976da72f895e70ae7ebb58d7298770b4974
Joel <58995538+jtpotato@users.noreply.github.com>
2024-11-17 18:17:45 +1100

    Add New Entry view with dismissal functionality to diary app

--- diaryapp-common/ContentView.swift
+++ diaryapp-common/ContentView.swift
@@ -31,7 +31,7 @@ struct ContentView: View {
         }
       }
       .sheet(isPresented: $isNewEntryPresented) {
-
+        NewEntry()
       }
     }
   }
--- diaryapp-common/NewEntry.swift
+++ diaryapp-common/NewEntry.swift
@@ -4,8 +4,23 @@ import SwiftUI
 struct NewEntry: View {
   @Environment(\.modelContext) private var modelContext
   @State private var diaryEntry = DiaryEntry.newEntryNow()
+  @Environment(\.presentationMode) private var presentationMode
 
   var body: some View {
+    ZStack {
+      Text("New Entry")
+        .bold()
+
+      HStack {
+        Spacer()
+        Button("Done") {
+          presentationMode.wrappedValue.dismiss()
+        }
+        .bold()
+        .padding()
+      }
+    }
+
     EntryEditor(entry: $diaryEntry)
       .onDisappear {
         if !diaryEntry.text.isEmpty {
