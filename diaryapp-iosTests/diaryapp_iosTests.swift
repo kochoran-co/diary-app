6cad194c56af9d6585312ca5dd5985dbdfe33066
Joel <58995538+jtpotato@users.noreply.github.com>
2024-11-17 18:11:57 +1100

    Add initial asset files and app structure for diary app

---
+++ diaryapp-iosTests/diaryapp_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  diaryapp_iosTests.swift
+//  diaryapp-iosTests
+//
+//  Created by Joel Tan on 17/11/2024.
+//
+
+import Testing
+@testable import diaryapp_ios
+
+struct diaryapp_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
